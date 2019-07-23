f5527f99b3253b531d4cc6b17d901b6488f47a07
Tapia_Timmothy <tapiatim24@gmail.com>
2019-07-23 12:06:28 -0400

    Finished Project

---
+++ HelloUnityWorld/Assets/Scripts/changeName.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class changeName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TMPro.TextMeshProUGUI myText = GetComponent<TextMeshProUGUI>();
+        if (Input.GetKeyDown ("space"))
+        {
+            myText.SetText("Hello World from Timmothy Tapia!");
+            Debug.Log ("Key Pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
